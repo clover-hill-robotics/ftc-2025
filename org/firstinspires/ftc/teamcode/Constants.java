531e83f5cf195610bdbbcb296f6c4bb78129593b
scientistofeverything <noah.e.haskins@gmail.com>
2025-04-12 15:31:10 -0400

    i let sammy use my computer
    his comments: "added Drivetrain and Scoring classes to Constants"

--- org/firstinspires/ftc/teamcode/Constants.java
+++ org/firstinspires/ftc/teamcode/Constants.java
@@ -16,40 +16,15 @@ public final class Constants {
 
     public static final class Drivetrain {
 
-        public static final double maxSpeed = 10.0;
-
-        public static final class LeftFront {
-
-            public static final double kP = 0.0;
-            public static final double kI = 0.0;
-            public static final double kD = 0.0;
-
-        }
-
-        public static final class RightFront {
-
-            public static final double kP = 0.0;
-            public static final double kI = 0.0;
-            public static final double kD = 0.0;
-
-        }
-
-        public static final class LeftRear {
-
-            public static final double kP = 0.0;
-            public static final double kI = 0.0;
-            public static final double kD = 0.0;
-
-        }
-
-        public static final class RightRear {
-
-            public static final double kP = 0.0;
-            public static final double kI = 0.0;
-            public static final double kD = 0.0;
-
-        }
+        public static final double maxSpeed = 1;
 
     }
 
+    public static final class Scoring {
+        //Arm Crap
+        public static final int maxRotation = 3000;
+        public static final int minRotation = 0;
+        public static final int maxSpeed = 0;
+        
+    }
 }
